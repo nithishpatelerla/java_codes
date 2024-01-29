dbdd0e59d21b4f9943d93245fca566597dab8462
nithishpatelerla <nithishpatelerla@gmail.com>
2024-01-29 22:46:00 +0530

    Cipher schools lecture codes upto 29th Jan

---
+++ Winter_Pep-master/Example.java
@@ -0,0 +1,7 @@
+class Example{
+    public static void main(String args[]){
+        System.out.println("hi this is main");
+        System.out.println("This is temp1");
+        System.out.println("This is in master");
+    }
+}
